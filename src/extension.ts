7c2e58d7168afeaf05256e5f815584654a26e9bc
Quinn Slack <qslack@qslack.com>
2018-09-09 17:06:51 -0700

    fix: use updated extension API

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,25 @@
 import { createWebWorkerMessageTransports } from 'sourcegraph/module/jsonrpc2/transports/webWorker'
-import {
-    SourcegraphExtensionAPI,
-    activateExtension,
-} from 'sourcegraph/module/extension'
+import { SourcegraphExtensionAPI, activateExtension } from 'sourcegraph'
 import { Settings, resolveSettings, resolveEndpoint } from './settings'
-import { combineLatest, from } from 'rxjs'
+import { combineLatest, from, ObservableInput } from 'rxjs'
 import {
     getFileCoverageRatios,
     getCommitCoverageRatio,
     getFileLineCoverage,
 } from './model'
 import { codecovToDecorations } from './decoration'
-import {
-    resolveURI,
-    ResolvedURI,
-    codecovParamsForRepositoryCommit,
-} from './uri'
+import { resolveURI, codecovParamsForRepositoryCommit } from './uri'
+import { Window } from 'sourcegraph/lib/extension/api'
 
 /** Entrypoint for the Codecov Sourcegraph extension. */
 export function run(sourcegraph: SourcegraphExtensionAPI<Settings>): void {
-    // The root URI of the repository that is open in the client (e.g., "git://github.com/my/repo?mysha").
-    const root: Pick<ResolvedURI, 'repo' | 'rev'> | null = sourcegraph.root
-        ? resolveURI(null, sourcegraph.root)
-        : null
-
     // When the configuration or current file changes, publish new decorations.
     //
     // TODO: Unpublish decorations on previously (but not currently) open files when settings changes, to avoid a
     // brief flicker of the old state when the file is reopened.
     combineLatest(
-        from(sourcegraph.configuration),
-        from(sourcegraph.windows)
+        from(sourcegraph.configuration as ObservableInput<Settings>),
+        from(sourcegraph.windows as ObservableInput<Window[]>)
     ).subscribe(async ([configuration, windows]) => {
         for (const window of windows) {
             // Publish the latest decorations for the file that's being displayed in the window, if any.
@@ -42,7 +31,7 @@ export function run(sourcegraph: SourcegraphExtensionAPI<Settings>): void {
                     codecovToDecorations(
                         settings,
                         await getFileLineCoverage(
-                            resolveURI(root, uri),
+                            resolveURI(uri),
                             settings['codecov.endpoints'][0]
                         )
                     )
@@ -55,50 +44,59 @@ export function run(sourcegraph: SourcegraphExtensionAPI<Settings>): void {
     // the "Coverage: N%" button label).
     //
     // The context only needs to be updated when the endpoints configuration changes.
-    sourcegraph.configuration
-        .watch('codecov.endpoints')
-        .subscribe(async configuration => {
-            if (!root) {
-                return
+    combineLatest(
+        from(sourcegraph.configuration.watch(
+            'codecov.endpoints'
+        ) as ObservableInput<Settings>),
+        from(sourcegraph.windows as ObservableInput<Window[]>)
+    ).subscribe(async ([configuration, windows]) => {
+        if (
+            !sourcegraph.activeWindow ||
+            !sourcegraph.activeWindow.activeComponent ||
+            !sourcegraph.activeWindow.activeComponent.resource
+        ) {
+            return
+        }
+        const lastURI = resolveURI(
+            sourcegraph.activeWindow.activeComponent.resource
+        )
+
+        const endpoint = resolveEndpoint(configuration['codecov.endpoints'])
+
+        const context: {
+            [key: string]: string | number | boolean | null
+        } = {}
+
+        const p = codecovParamsForRepositoryCommit(lastURI)
+        // TODO Support non-codecov.io endpoints.
+        const repoURL = `https://codecov.io/${p.service}/${p.owner}/${p.repo}`
+        context['codecov.repoURL'] = repoURL
+        const baseFileURL = `${repoURL}/src/${p.sha}`
+        context['codecov.commitURL'] = `${repoURL}/commit/${p.sha}`
+
+        try {
+            // Store overall commit coverage ratio.
+            const commitCoverage = await getCommitCoverageRatio(
+                lastURI,
+                endpoint
+            )
+            context['codecov.commitCoverage'] = commitCoverage
+                ? commitCoverage.toFixed(1)
+                : null
+
+            // Store coverage ratio (and Codecov report URL) for each file at this commit so that
+            // template strings in contributions can refer to these values.
+            const fileRatios = await getFileCoverageRatios(lastURI, endpoint)
+            for (const [path, ratio] of Object.entries(fileRatios)) {
+                const uri = `git://${lastURI.repo}?${lastURI.rev}#${path}`
+                context[`codecov.coverageRatio.${uri}`] = ratio.toFixed(0)
+                context[`codecov.fileURL.${uri}`] = `${baseFileURL}/${path}`
             }
-            const endpoint = resolveEndpoint(configuration['codecov.endpoints'])
-
-            const context: {
-                [key: string]: string | number | boolean | null
-            } = {}
-
-            const p = codecovParamsForRepositoryCommit(root)
-            // TODO Support non-codecov.io endpoints.
-            const repoURL = `https://codecov.io/${p.service}/${p.owner}/${
-                p.repo
-            }`
-            context['codecov.repoURL'] = repoURL
-            const baseFileURL = `${repoURL}/src/${p.sha}`
-            context['codecov.commitURL'] = `${repoURL}/commit/${p.sha}`
-
-            try {
-                // Store overall commit coverage ratio.
-                const commitCoverage = await getCommitCoverageRatio(
-                    root,
-                    endpoint
-                )
-                context['codecov.commitCoverage'] = commitCoverage
-                    ? commitCoverage.toFixed(1)
-                    : null
-
-                // Store coverage ratio (and Codecov report URL) for each file at this commit so that
-                // template strings in contributions can refer to these values.
-                const fileRatios = await getFileCoverageRatios(root, endpoint)
-                for (const [path, ratio] of Object.entries(fileRatios)) {
-                    const uri = `git://${root.repo}?${root.rev}#${path}`
-                    context[`codecov.coverageRatio.${uri}`] = ratio.toFixed(0)
-                    context[`codecov.fileURL.${uri}`] = `${baseFileURL}/${path}`
-                }
-            } catch (err) {
-                console.error(`Error loading Codecov file coverage: ${err}`)
-            }
-            sourcegraph.context.updateContext(context)
-        })
+        } catch (err) {
+            console.error(`Error loading Codecov file coverage: ${err}`)
+        }
+        sourcegraph.context.updateContext(context)
+    })
 
     // Handle the "Set Codecov API token" command (show the user a prompt for their token, and save
     // their input to settings).
--- src/uri.ts
+++ src/uri.ts
@@ -13,10 +13,7 @@ export interface ResolvedURI {
  * Resolve a URI of the forms git://github.com/owner/repo?rev#path and file:///path to an absolute reference, using
  * the given base (root) URI.
  */
-export function resolveURI(
-    base: Pick<ResolvedURI, 'repo' | 'rev'> | null,
-    uri: string
-): ResolvedURI {
+export function resolveURI(uri: string): ResolvedURI {
     const url = new URL(uri)
     if (url.protocol === 'git:') {
         return {
@@ -25,16 +22,8 @@ export function resolveURI(
             path: url.hash.slice(1),
         }
     }
-    if (url.protocol === 'file:') {
-        if (!base) {
-            throw new Error(`unable to resolve URI ${uri} with no base`)
-        }
-        return { ...base, path: url.pathname }
-    }
     throw new Error(
-        `unrecognized URI: ${JSON.stringify(
-            uri
-        )} (supported URI schemes: git, file)`
+        `unrecognized URI: ${JSON.stringify(uri)} (supported URI schemes: git)`
     )
 }
 
